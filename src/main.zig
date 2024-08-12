6f551823d4f561945c7a1d7eeed04d1ee93bb861
prodbysky <kyrbynyt@gmail.com>
2024-08-12 20:41:59 +0300

    Full movement implemented

--- src/main.zig
+++ src/main.zig
@@ -1,25 +1,39 @@
 const std = @import("std");
 const rl = @import("raylib");
 
-const Player = struct {
-    color: rl.Color = .{ .r = 0xdd, .g = 0, .b = 0xee, .a = 0xff },
-    pos: rl.Vector2 = .{ .x = 0, .y = 0 },
-    size: rl.Vector2 = .{ .x = 64, .y = 64 },
-
-    pub fn draw(self: *const Player) void {
-        rl.drawRectangleV(self.pos, self.size, self.color);
-    }
-};
-
 pub fn main() !void {
     rl.initWindow(640, 480, "Hello Zig!");
     defer rl.closeWindow();
 
-    const player = Player{ .pos = .{ .x = 100, .y = 100 }, .size = .{ .x = 100, .y = 100 } };
+    var paddle = Paddle{ .pos = 100 };
 
     while (!rl.windowShouldClose()) {
+        paddle.update();
         rl.beginDrawing();
-        player.draw();
+        rl.clearBackground(rl.getColor(0x181818ff));
+        paddle.draw();
         rl.endDrawing();
     }
 }
+
+const Paddle = struct {
+    const speed: u16 = 400;
+    const y_pos: f32 = 400;
+    const color: rl.Color = .{ .r = 0xdd, .g = 0, .b = 0xee, .a = 0xff };
+    const size: rl.Vector2 = .{ .x = 128, .y = 32 };
+    pos: f32 = 0,
+
+    pub fn draw(self: *const Paddle) void {
+        rl.drawRectangleV(.{ .x = self.pos, .y = Paddle.speed }, Paddle.size, Paddle.color);
+    }
+
+    pub fn update(self: *Paddle) void {
+        if (rl.isKeyDown(.key_a)) {
+            self.pos -= Paddle.speed * rl.getFrameTime();
+        }
+        if (rl.isKeyDown(.key_d)) {
+            self.pos += Paddle.speed * rl.getFrameTime();
+        }
+        self.pos = std.math.clamp(self.pos, 0, 640 - Paddle.size.x);
+    }
+};
